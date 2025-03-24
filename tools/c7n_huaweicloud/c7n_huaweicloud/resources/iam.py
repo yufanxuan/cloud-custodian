49a3cf40da4667abec360101e31792f8abcf69e2
wangxianghui <2803143498@qq.com>
2025-03-24 21:54:20 +0800

    add iam-policy

--- tools/c7n_huaweicloud/c7n_huaweicloud/resources/iam.py
+++ tools/c7n_huaweicloud/c7n_huaweicloud/resources/iam.py
@@ -46,7 +46,7 @@ class IAMMarkerPagination(Pagination):
 class User(QueryResourceManager):
 
     class resource_type(TypeInfo):
-        service = 'iam-policy'
+        service = 'iam'
         pagination = IAMMarkerPagination()
         enum_spec = ("list_users_v5", 'users', pagination)
         id = 'user_id'
@@ -56,7 +56,7 @@ class User(QueryResourceManager):
 class Policy(QueryResourceManager):
 
     class resource_type(TypeInfo):
-        service = 'iam'
+        service = 'iam-policy'
         pagination = IAMMarkerPagination()
         enum_spec = ("list_policies_v5", 'policies', pagination)
         id = 'policy_id'
