0daadb2745a5ee4acd64757dcd23c71be7a07c0f
Scout-92 <984493542@qq.com>
2025-03-27 16:47:10 +0800

    iam commit1

--- tools/c7n_huaweicloud/tests/test_iam.py
+++ tools/c7n_huaweicloud/tests/test_iam.py
@@ -1,8 +1,24 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+import os
+
 from huaweicloud_common import BaseTest
 
+HUAWEICLOUD_CONFIG_GLOBAL = {
+    'HUAWEI_DEFAULT_REGION': 'cn-north-4',
+    'HUAWEI_ACCESS_KEY_ID': 'access_key_id',
+    'HUAWEI_SECRET_ACCESS_KEY': 'secret_access_key',
+    'HUAWEI_PROJECT_ID': 'cn-north-4',
+}
+
+
+def init_huaweicloud_config_global():
+    for k, v in HUAWEICLOUD_CONFIG_GLOBAL.items():
+        os.environ[k] = v
 
 class IamTest(BaseTest):
     def test_alarm_query(self):
+        init_huaweicloud_config_global()
         factory = self.replay_flight_data('iam_user_delete')
         p = self.load_policy({
             'name': 'delete-user',
