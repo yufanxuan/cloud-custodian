0ccc87dd491db95fbb0e88efd2bcb73ade1ec51a
Scout-92 <984493542@qq.com>
2025-06-20 16:25:15 +0800

    c7n-org run-script feature

--- tools/c7n_huaweicloud/tests/data/iam_test/script_test.py
+++ tools/c7n_huaweicloud/tests/data/iam_test/script_test.py
@@ -7,14 +7,13 @@ log = logging.getLogger("c7n-org.script_test.py")
 
 def main():
     print("Script execution start.")
-    print(f"account: {os.getenv("HUAWEICLOUD_ACCESS_KEY_ID")}, "
-          f"{os.getenv("HUAWEICLOUD_SECRET_ACCESS_KEY")}, "
-          f"{os.getenv("HUAWEICLOUD_REGION")}, "
-          f"{os.getenv("HUAWEICLOUD_DOMAIN_NAME")}, "
-          f"{os.getenv("HUAWEICLOUD_DOMAIN_ID")}, "
-          f"{os.getenv("HUAWEICLOUD_DOMAIN_STATUS")}, "
-          f"{os.getenv("HUAWEICLOUD_SECURITY_TOKEN")}")
-
+    print(f"account region: {os.getenv("HUAWEICLOUD_REGION")}")
+    print(f"account name: {os.getenv("HUAWEICLOUD_DOMAIN_NAME")}")
+    print(f"account id: {os.getenv("HUAWEICLOUD_DOMAIN_ID")}")
+    print(f"account status: {os.getenv("HUAWEICLOUD_DOMAIN_STATUS")}")
+    print(f"account ak: {os.getenv("HUAWEICLOUD_ACCESS_KEY_ID")}")
+    print(f"account sk: {os.getenv("HUAWEICLOUD_SECRET_ACCESS_KEY")}")
+    print(f"account token: {os.getenv("HUAWEICLOUD_SECURITY_TOKEN")}")
     print("Script execution completed")
     return 0
 
